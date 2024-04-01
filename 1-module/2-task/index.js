2fa1374e7f8895f8070e82398ee19cc6a24282d3
tokmashik <tokmashik2@gmail.com>
2024-04-01 22:19:22 +0700

    add 2 task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return name.length >= 4 && !name.includes(" ") ? true : false;
+  return name !== null && name.length >= 4 && !name.includes(" ") ? true : false;
 }
 
 function sayHello() {
